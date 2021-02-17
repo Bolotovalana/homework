18141be60be023ab54b9a378fd9ec98b9f5e3d6e
Svetlana Bolotova <lanasqa@gmail.com>
2021-02-16 21:06:47 -0500

    add homeworks from 1.1 to 3.4

---
+++ task_1_3.py
@@ -0,0 +1,9 @@
+
+# Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.
+
+edge = 3
+square = 6 * edge**2
+volume = edge**3
+
+print(square)
+print(volume)
---
+++ task_1_5.py
@@ -0,0 +1,9 @@
+# Даны катеты прямоугольного треугольника. Найти его гипотенузу и площадь.
+
+leg1 = 3
+leg2 = 4
+
+hypotenuse = (leg1**2 + leg2**2)**0.5
+square = 1/2*(leg1 * leg2)
+print(hypotenuse)
+print(square)
