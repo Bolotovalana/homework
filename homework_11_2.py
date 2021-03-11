57643adfb177f94742c6298141d2b150b762ef84
Svetlana Bolotova <lanasqa@gmail.com>
2021-03-11 10:42:10 -0500

    update homework_11_2.py

--- homework_11_2.py
+++ homework_11_2.py
@@ -1,36 +1,52 @@
 # 2. Создать класс Car. Атрибуты: марка, модель, год выпуска, скорость(по умолчанию 0).
 # Методы: увеличить скорости(скорость + 5), уменьшение скорости(скорость - 5),
 # стоп(сброс скорости на 0), отображение скорости, разворот(изменение знака скорости). Все атрибуты приватные.
-
-def slow_down(_speed):
-    if _speed != 0:
-        return _speed - 5
-    else:
-        print("Car is already stopped")
-
-
+# 4. Просмотреть ваше задание, где вы реализовывали класс Car и подумать,
+# где стоит добавить выкидывание(raise) исключений(например, когда скорость может стать меньше 0).
+# В месте, где вы работаете с этим классом , добавьте обработку этих исключений, используя конструкцию try.
 class Car:
-    def __init__(self, __make, __model, __year, __speed=0):
-        self.__make = __make
-        self.__model = __model
-        self.__year = __year
-        self.__speed = __speed
+    def __init__(self, _make, _model, _year):
+        self._make = _make
+        self._model = _model
+        self._year = _year
+        self._speed = 0
 
-    def accelerate(self, __speed):
-        return __speed + 5
+    def accelerate(self):
+        self._speed = self._speed + 5
 
-    def stopping(self, __speed):
-        while __speed > 0:
-            slow_down()
-            Car.show_speed(__speed)
+    def slow_down(self):
+        if self._speed != 0:
+            if (self._speed - 5) < 0:
+                raise Exception("Скорости меньше нуля не бывает!")
+            else:
+                self._speed = self._speed - 5
+        else:
+            self._speed = 0
+            print("Car is already stopped")
 
-    def show_speed(self, __speed):
-        print(__speed)
+    def stop(self):
+        while self._speed > 0:
+            Car.slow_down(self._speed)
+            Car.show_speed(self._speed)
 
-    def u_turn(self, __speed):
-        while __speed > 15:
-            slow_down()
+    def show_speed(self):
+        print(self._speed)
+
+    def u_turn(self):
+        while self._speed > 15:
+            Car.slow_down(self._speed)
             Car.show_speed()
-        while __speed < 45:
-            Car.accelerate(__speed)
+        while self._speed < 45:
+            Car.accelerate(self._speed)
             Car.show_speed()
+
+
+car = Car("M", "s", 1998)
+car.show_speed()
+car.accelerate()
+# car.accelerate()
+car.show_speed()
+car.slow_down()
+car.slow_down()
+car.slow_down()
+car.show_speed()
